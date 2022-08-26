5cdd553f4284fc8058885f904f1aa9f59306d32e
Jim Hess <jihesspastor@yahoo.com>
2022-08-26 07:00:18 -0400

    Changed EC2 Instances from 2 to 3

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "my-ec2-vm" {
   ami                    = data.aws_ami.amzLinux.id
   instance_type          = var.ec2_instance_type
   key_name               = "MyKeyPair"
-  count                  = 2
+  count                  = 3
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   # user_data              = file("apache-install.sh")
   user_data = file("apache-install.sh")
