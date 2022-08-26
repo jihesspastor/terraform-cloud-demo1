febe51cf227039185c935cfb76ec4f1241819816
Jim Hess <jihesspastor@yahoo.com>
2022-08-26 06:31:05 -0400

    Changed the Key Pair Name from MyFrankfurtKeyPair to MyKeyPair

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "my-ec2-vm" {
   ami                    = data.aws_ami.amzLinux.id
   instance_type          = var.ec2_instance_type
-  key_name               = "FrankfurtKeyPair"
+  key_name               = "MyKeyPair"
   count                  = 1
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   # user_data              = file("apache-install.sh")
